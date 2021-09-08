1321be9a075143460a8ad5641442e13d6360690f
do-rh <87504110+do-rh@users.noreply.github.com>
2021-09-08 12:21:49 -0700

    completed

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,47 @@
-# Put your app in here.
+from flask import Flask, request
+import operations
+
+app = Flask(__name__)
+
+
+@app.get('/add')
+def addition():
+    """Return sum of query string args"""
+    [a, b] = [request.args["a"], request.args["b"]]
+    return str(operations.add(int(a), int(b)))
+
+
+@app.get('/sub')
+def subtraction():
+    """Return difference of query string args"""
+    [a, b] = [request.args["a"], request.args["b"]]
+    return str(operations.sub(int(a), int(b)))
+
+
+@app.get('/mult')
+def multiplication():
+    """Return product of query string args"""
+    [a, b] = [request.args["a"], request.args["b"]]
+    return str(operations.mult(int(a), int(b)))
+
+
+@app.get('/div')
+def division():
+    """Return quotient of query string args"""
+    [a, b] = [request.args["a"], request.args["b"]]
+    return str(operations.div(int(a), int(b)))
+
+
+@app.get('/math/<operation>')
+def math(operation):
+    """Return result of chosen math operation from the query string args"""
+    [a, b] = [int(request.args["a"]), int(request.args["b"])]
+
+    op_query = {
+        "add": operations.add,
+        "sub": operations.sub,
+        "mult": operations.mult,
+        "div": operations.div
+    }
+
+    return str(op_query[operation](a, b))
